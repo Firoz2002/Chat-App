194e362bd150608b830f1f44e7e8a557fb8ba174
FirozKamdar <105146975+Firoz2002@users.noreply.github.com>
2024-05-08 10:35:08 +0530

    commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 const connect = async () => {
-    await mongoose.connect('mongodb://localhost/chat_app');
+    try {
+        await mongoose.connect("mongodb+srv://firozkamdar1:MP11NC4201@cluster1.s5t0hag.mongodb.net/?retryWrites=true&w=majority");
+    } catch (error) {
+        throw {error}
+    }
 }
 
 module.exports = connect;
